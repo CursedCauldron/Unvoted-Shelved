3a24d1b7ee0d8c4330d9e087cfcf881832c157f1
dopadream <slyshidiscord@gmail.com>
2021-10-25 14:28:35 -0500

    glare spawnegg

--- src/main/java/com/cursedcauldron/unvotedandshelved/core/UnvotedAndShelved.java
+++ src/main/java/com/cursedcauldron/unvotedandshelved/core/UnvotedAndShelved.java
@@ -11,8 +11,12 @@ import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.object.builder.v1.entity.FabricDefaultAttributeRegistry;
 import net.minecraft.entity.ai.brain.Activity;
 import net.minecraft.entity.ai.brain.MemoryModuleType;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.SpawnEggItem;
 import net.minecraft.sound.SoundEvent;
 import net.minecraft.util.Identifier;
+import net.minecraft.util.registry.Registry;
 
 //<>
 
@@ -21,15 +25,14 @@ public class UnvotedAndShelved implements ModInitializer {
     public static Activity FIND_DARKNESS = ActivityInvoker.invokeRegister("find_darkness");
     public static MemoryModuleType<Integer> SEEK_TICKS = MemoryInvoker.invokeRegister("seek_ticks", Codec.INT);
     public static MemoryModuleType<Integer> GRUMPY_TICKS = MemoryInvoker.invokeRegister("grumpy_ticks", Codec.INT);
-
+    public static final Item GLARE_SPAWN_EGG = new SpawnEggItem(USEntities.GLARE, 7837492, 5204011, new Item.Settings().group(ItemGroup.MISC));
 
     @Override
     public void onInitialize() {
         SoundRegistry.init();
-        Reflection.initialize(
-                USEntities.class
-        );
+        Reflection.initialize(USEntities.class);
 
+        Registry.register(Registry.ITEM, new Identifier(MODID, "glare_spawn_egg"), GLARE_SPAWN_EGG);
 
         FabricDefaultAttributeRegistry.register(USEntities.GLARE, GlareEntity.createGlareAttributes());
     }
