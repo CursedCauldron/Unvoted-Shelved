a3aca38ba66ac20ccb3fda2030e5a8eedc450e9b
dopadream <slyshidiscord@gmail.com>
2021-12-07 18:54:51 -0600

    fuck

--- src/main/java/com/cursedcauldron/unvotedandshelved/common/entity/ai/task/GlowberryStrollTask.java
+++ src/main/java/com/cursedcauldron/unvotedandshelved/common/entity/ai/task/GlowberryStrollTask.java
@@ -52,7 +52,7 @@ public class GlowberryStrollTask extends Behavior<GlareEntity> {
     }
 
     private boolean isValidSpawnPos(BlockPos blockPos, ServerLevel level) {
-        return !level.getBlockState(blockPos).is(Blocks.VINE) && !level.getBlockState(blockPos).is(BlockTags.LEAVES) && !level.getBlockState(blockPos).isAir() && !level.getFluidState(blockPos).is(Fluids.WATER) && !level.getFluidState(blockPos).is(Fluids.FLOWING_WATER);
+        return !level.getBlockState(blockPos).is(Blocks.VINE) && !level.getBlockState(blockPos).is(BlockTags.LEAVES) &&  !level.getBlockState(blockPos).is(BlockTags.CAVE_VINES) && !level.getBlockState(blockPos).isAir() && !level.getFluidState(blockPos).is(Fluids.WATER) && !level.getFluidState(blockPos).is(Fluids.FLOWING_WATER);
     }
 
     protected void getDarkPos(ServerLevel level, GlareEntity glare) {
