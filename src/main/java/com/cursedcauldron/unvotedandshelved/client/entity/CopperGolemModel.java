00e7be929292a05a68b1ae7466ba1bec37267d12
dopadream <slyshidiscord@gmail.com>
2022-04-09 15:20:20 -0500

    no horns

--- src/main/java/com/cursedcauldron/unvotedandshelved/client/entity/CopperGolemModel.java
+++ src/main/java/com/cursedcauldron/unvotedandshelved/client/entity/CopperGolemModel.java
@@ -14,19 +14,9 @@ import net.minecraft.client.model.geom.builders.PartDefinition;
 @Environment(EnvType.CLIENT)
 public class CopperGolemModel<T extends CopperGolemEntity> extends HierarchicalModel<T> {
     private final ModelPart root;
-    private final ModelPart head;
-    private final ModelPart rightArm;
-    private final ModelPart leftArm;
-    private final ModelPart rightLeg;
-    private final ModelPart leftLeg;
 
     public CopperGolemModel(ModelPart part) {
         this.root = part;
-        this.head = part.getChild("head");
-        this.rightArm = part.getChild("right_arm");
-        this.leftArm = part.getChild("left_arm");
-        this.rightLeg = part.getChild("right_leg");
-        this.leftLeg = part.getChild("left_leg");
     }
 
     public static LayerDefinition getLayerDefinition() {
@@ -35,19 +25,13 @@ public class CopperGolemModel<T extends CopperGolemEntity> extends HierarchicalM
 
         PartDefinition body = partData.addOrReplaceChild("head", CubeListBuilder.create().texOffs(0, 21).addBox(-6.0F, -4.0F, -4.0F, 12.0F, 8.0F, 8.0F), PartPose.offset(0.0F, 15.0F, 0.0F));
 
-        PartDefinition head = body.addOrReplaceChild("head", CubeListBuilder.create().texOffs(0, 0).addBox(-7.0F, -9.0F, -6.0F, 14.0F, 9.0F, 12.0F).texOffs(0, 21).addBox(-1.0F, -12.0F, -1.0F, 2.0F, 3.0F, 2.0F).texOffs(32, 21).addBox(-2.0F, -16.0F, -2.0F, 4.0F, 4.0F, 4.0F).texOffs(0, 0).addBox(-2.0F, -5.0F, -8.0F, 4.0F, 6.0F, 2.0F), PartPose.offset(0.0F, -4.0F, 0.0F));
 
-        PartDefinition rightArm = body.addOrReplaceChild("right_arm", CubeListBuilder.create().texOffs(0, 37).addBox(-1.5F, 0.0F, -1.5F, 3.0F, 12.0F, 3.0F), PartPose.offset(-7.5F, -4.0F, 0.0F));
-        PartDefinition leftArm = body.addOrReplaceChild("left_arm", CubeListBuilder.create().texOffs(0, 37).addBox(-1.5F, 0.0F, -1.5F, 3.0F, 12.0F, 3.0F).mirror(false), PartPose.offset(7.5F, -4.0F, 0.0F));
-        PartDefinition rightLeg = body.addOrReplaceChild("right_leg", CubeListBuilder.create().texOffs(36, 33).addBox(-2.0F, 0.0F, -2.0F, 4.0F, 5.0F, 4.0F), PartPose.offset(-3.0F, 4.0F, 0.0F));
-        PartDefinition leftLeg = body.addOrReplaceChild("left_leg", CubeListBuilder.create().texOffs(36, 33).addBox(-2.0F, 0.0F, -2.0F, 4.0F, 5.0F, 4.0F).mirror(false), PartPose.offset(3.0F, 4.0F, 0.0F));
+        body.addOrReplaceChild("head", CubeListBuilder.create().texOffs(0, 0).addBox(-7.0F, -9.0F, -6.0F, 14.0F, 9.0F, 12.0F).texOffs(0, 21).addBox(-1.0F, -12.0F, -1.0F, 2.0F, 3.0F, 2.0F).texOffs(32, 21).addBox(-2.0F, -16.0F, -2.0F, 4.0F, 4.0F, 4.0F).texOffs(0, 0).addBox(-2.0F, -5.0F, -8.0F, 4.0F, 6.0F, 2.0F), PartPose.offset(0.0F, -4.0F, 0.0F));
+        body.addOrReplaceChild("right_arm", CubeListBuilder.create().texOffs(0, 37).addBox(-1.5F, 0.0F, -1.5F, 3.0F, 12.0F, 3.0F), PartPose.offset(-7.5F, -4.0F, 0.0F));
+        body.addOrReplaceChild("left_arm", CubeListBuilder.create().texOffs(0, 37).addBox(-1.5F, 0.0F, -1.5F, 3.0F, 12.0F, 3.0F).mirror(false), PartPose.offset(7.5F, -4.0F, 0.0F));
+        body.addOrReplaceChild("right_leg", CubeListBuilder.create().texOffs(36, 33).addBox(-2.0F, 0.0F, -2.0F, 4.0F, 5.0F, 4.0F), PartPose.offset(-3.0F, 4.0F, 0.0F));
+        body.addOrReplaceChild("left_leg", CubeListBuilder.create().texOffs(36, 33).addBox(-2.0F, 0.0F, -2.0F, 4.0F, 5.0F, 4.0F).mirror(false), PartPose.offset(3.0F, 4.0F, 0.0F));
 
-        partData.addOrReplaceChild("head", CubeListBuilder.create().texOffs(0, 0).addBox(-7.0F, -9.0F, -6.0F, 14.0F, 9.0F, 12.0F).texOffs(0, 21).addBox(-1.0F, -12.0F, -1.0F, 2.0F, 3.0F, 2.0F).texOffs(32, 21).addBox(-2.0F, -16.0F, -2.0F, 4.0F, 4.0F, 4.0F).texOffs(0, 0).addBox(-2.0F, -5.0F, -8.0F, 4.0F, 6.0F, 2.0F), PartPose.offset(0.0F, -4.0F, 0.0F));
-        partData.addOrReplaceChild("head", CubeListBuilder.create().texOffs(0, 21).addBox(-6.0F, -4.0F, -4.0F, 12.0F, 8.0F, 8.0F), PartPose.offset(0.0F, 15.0F, 0.0F));
-        partData.addOrReplaceChild("right_arm", CubeListBuilder.create().texOffs(0, 37).addBox(-1.5F, 0.0F, -1.5F, 3.0F, 12.0F, 3.0F), PartPose.offset(-7.5F, -4.0F, 0.0F));
-        partData.addOrReplaceChild("left_arm", CubeListBuilder.create().texOffs(0, 37).addBox(-1.5F, 0.0F, -1.5F, 3.0F, 12.0F, 3.0F).mirror(false), PartPose.offset(7.5F, -4.0F, 0.0F));
-        partData.addOrReplaceChild("right_leg", CubeListBuilder.create().texOffs(36, 33).addBox(-2.0F, 0.0F, -2.0F, 4.0F, 5.0F, 4.0F), PartPose.offset(-3.0F, 4.0F, 0.0F));
-        partData.addOrReplaceChild("left_leg", CubeListBuilder.create().texOffs(36, 33).addBox(-2.0F, 0.0F, -2.0F, 4.0F, 5.0F, 4.0F).mirror(false), PartPose.offset(3.0F, 4.0F, 0.0F));
         return LayerDefinition.create(data, 64, 64);
     }
 
