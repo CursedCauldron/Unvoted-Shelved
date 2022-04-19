05b97aa947ec451a75c40f8c519f1a70507ca9ac
dopadream <slyshidiscord@gmail.com>
2022-04-19 05:10:29 -0500

    ZA HANDO, ERASE THAT BAD CODE!

--- src/main/java/com/cursedcauldron/unvotedandshelved/common/entity/ai/copper_golem/task/PressCopperButtonTask.java
+++ src/main/java/com/cursedcauldron/unvotedandshelved/common/entity/ai/copper_golem/task/PressCopperButtonTask.java
@@ -7,7 +7,6 @@ import com.cursedcauldron.unvotedandshelved.core.registries.USMemoryModules;
 import com.cursedcauldron.unvotedandshelved.core.registries.USSounds;
 import com.google.common.collect.ImmutableMap;
 import net.minecraft.core.BlockPos;
-import net.minecraft.core.Direction;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.sounds.SoundSource;
 import net.minecraft.util.valueproviders.UniformInt;
@@ -15,14 +14,13 @@ import net.minecraft.world.entity.Pose;
 import net.minecraft.world.entity.ai.behavior.Behavior;
 import net.minecraft.world.entity.ai.memory.MemoryModuleType;
 import net.minecraft.world.entity.ai.memory.MemoryStatus;
-import net.minecraft.world.level.block.ButtonBlock;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.block.state.properties.AttachFace;
 
 import java.util.Optional;
 
 public class PressCopperButtonTask extends Behavior<CopperGolemEntity> {
-    private int pressingTicks;
+    private int buttonTicks;
 
     public PressCopperButtonTask() {
         super(ImmutableMap.of(MemoryModuleType.LOOK_TARGET, MemoryStatus.VALUE_PRESENT, USMemoryModules.COPPER_BUTTON, MemoryStatus.VALUE_PRESENT));
@@ -30,12 +28,12 @@ public class PressCopperButtonTask extends Behavior<CopperGolemEntity> {
 
     @Override
     protected boolean checkExtraStartConditions(ServerLevel world, CopperGolemEntity entity) {
-        return entity.getBrain().getMemory(USMemoryModules.COPPER_BUTTON_COOLDOWN_TICKS).isEmpty();
+        return entity.getBrain().getMemory(USMemoryModules.COPPER_BUTTON).isPresent() && entity.getBrain().getMemory(USMemoryModules.COPPER_BUTTON_COOLDOWN_TICKS).isEmpty();
     }
 
     @Override
     protected boolean canStillUse(ServerLevel world, CopperGolemEntity entity, long p_22547_) {
-        return entity.getStage() != CopperGolemEntity.Stage.OXIDIZED && this.pressingTicks < 200;
+        return entity.getBrain().getMemory(USMemoryModules.COPPER_BUTTON).isPresent() && entity.getBrain().getMemory(USMemoryModules.COPPER_BUTTON_COOLDOWN_TICKS).isEmpty();
     }
 
     @Override
@@ -44,7 +42,7 @@ public class PressCopperButtonTask extends Behavior<CopperGolemEntity> {
         optional.ifPresent(blockPos -> {
             BlockState state = entity.level.getBlockState(blockPos);
             boolean flag = entity.blockPosition().closerThan(blockPos, 2);
-            if (this.pressingTicks < 200 && flag && state.getBlock() instanceof CopperButtonBlock buttonBlock) {
+            if (flag && state.getBlock() instanceof CopperButtonBlock buttonBlock) {
                 AttachFace direction = state.getValue(CopperButtonBlock.FACE);
                 entity.getLookControl().setLookAt(blockPos.getX(), blockPos.getY(), blockPos.getZ());
                 if (direction == AttachFace.FLOOR) {
@@ -64,20 +62,22 @@ public class PressCopperButtonTask extends Behavior<CopperGolemEntity> {
 
     @Override
     protected void tick(ServerLevel world, CopperGolemEntity entity, long p_22553_) {
-        if (this.pressingTicks < 200) {
-            this.pressingTicks++;
+        if (this.buttonTicks < 60) {
+            this.buttonTicks++;
+        } else {
+            entity.getBrain().eraseMemory(USMemoryModules.COPPER_BUTTON);
+            entity.setCooldown();
+            entity.setPose(Pose.STANDING);
         }
-
-        System.out.println("The pressing ticks are " + this.pressingTicks);
+        System.out.println("The button ticks are " + this.buttonTicks);
     }
+
 
     @Override
     protected void stop(ServerLevel world, CopperGolemEntity entity, long p_22550_) {
         System.out.println("Task is stopping...");
-        if (this.pressingTicks >= 1) {
-            this.pressingTicks = 0;
-            entity.getBrain().eraseMemory(USMemoryModules.COPPER_BUTTON);
-            entity.setCooldown();
+        if (this.buttonTicks >= 1) {
+            this.buttonTicks = 0;
             entity.setPose(Pose.STANDING);
         }
     }
