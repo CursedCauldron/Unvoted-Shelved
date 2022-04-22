68dc58062e4175947eb643f9288c722d9bc266f2
BlackGear27 <itsblackgear@gmail.com>
2022-04-22 18:02:00 -0400

    don't hit me steph

---
+++ src/main/java/com/cursedcauldron/unvotedandshelved/mixin/access/ItemAccessor.java
@@ -0,0 +1,13 @@
+package com.cursedcauldron.unvotedandshelved.mixin.access;
+
+import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.Item;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Mutable;
+import org.spongepowered.asm.mixin.gen.Accessor;
+
+@Mixin(Item.class)
+public interface ItemAccessor {
+    @Mutable @Accessor
+    void setCategory(CreativeModeTab category);
+}
