6f942df536e7aeba04c2baf6e167aad4460837a5
dopadream <slyshidiscord@gmail.com>
2022-04-25 01:37:20 -0500

    stuff that doesn't work

--- src/main/java/com/cursedcauldron/unvotedandshelved/common/blocks/WeatheringRotatedPillarBlock.java
+++ src/main/java/com/cursedcauldron/unvotedandshelved/common/blocks/WeatheringRotatedPillarBlock.java
@@ -5,11 +5,13 @@ import com.google.common.base.Suppliers;
 import com.google.common.collect.BiMap;
 import com.google.common.collect.ImmutableBiMap;
 import net.minecraft.core.BlockPos;
+import net.minecraft.core.Direction;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.RotatedPillarBlock;
 import net.minecraft.world.level.block.WeatheringCopper;
 import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.block.state.properties.Property;
 
 import java.util.Optional;
 import java.util.Random;
@@ -19,6 +21,8 @@ import java.util.function.Supplier;
 
 public class WeatheringRotatedPillarBlock extends RotatedPillarBlock implements WeatheringCopper {
     private final WeatheringCopper.WeatherState weatherState;
+    private WeatheringCopper.WeatherState connectionState;
+
     public static Supplier<BiMap<Block, Block>> NEXT_BY_BLOCK = Suppliers.memoize(() -> {
         return ImmutableBiMap.<Block, Block>builder()
                 .put(USBlocks.COPPER_PILLAR, USBlocks.EXPOSED_COPPER_PILLAR)
@@ -30,14 +34,31 @@ public class WeatheringRotatedPillarBlock extends RotatedPillarBlock implements 
         return NEXT_BY_BLOCK.get().inverse();
     });
 
-    public WeatheringRotatedPillarBlock(WeatherState state, Properties properties) {
+    public WeatheringRotatedPillarBlock(WeatherState state, WeatherState connectionState, Properties properties) {
         super(properties);
         this.weatherState = state;
+        this.connectionState = connectionState;
     }
 
     @Override
     public void randomTick(BlockState state, ServerLevel level, BlockPos pos, Random random) {
         this.onRandomTick(state, level, pos, random);
+    }
+
+    @Override
+    public void tick(BlockState state, ServerLevel level, BlockPos pos, Random random) {
+        if (this.weatherState == WeatherState.UNAFFECTED) {
+            if (state.getValue(AXIS) == Direction.Axis.Y) {
+                BlockState belowState = level.getBlockState(pos.below());
+                if (belowState.is(USBlocks.EXPOSED_COPPER_PILLAR)) {
+                    level.setBlockAndUpdate(pos, USBlocks.COPPER_PILLAR_CONNECTED_EXPOSED.defaultBlockState());
+                } else if (belowState.is(USBlocks.WEATHERED_COPPER_PILLAR)) {
+                    level.setBlockAndUpdate(pos, USBlocks.COPPER_PILLAR_CONNECTED_WEATHERED.defaultBlockState());
+                } else if (belowState.is(USBlocks.OXIDIZED_COPPER_PILLAR)) {
+                    level.setBlockAndUpdate(pos, USBlocks.COPPER_PILLAR_CONNECTED_OXIDIZED.defaultBlockState());
+                }
+            }
+        }
     }
 
     @Override
