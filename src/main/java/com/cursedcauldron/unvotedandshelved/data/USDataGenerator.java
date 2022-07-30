32acbf68b0c915ca1f1e5c1a005d701953265ee8
ExDrill <72329798+ExDrill@users.noreply.github.com>
2022-07-29 20:15:44 -0500

    Update to 41.1.0

--- src/main/java/com/cursedcauldron/unvotedandshelved/data/USDataGenerator.java
+++ src/main/java/com/cursedcauldron/unvotedandshelved/data/USDataGenerator.java
@@ -3,9 +3,9 @@ package com.cursedcauldron.unvotedandshelved.data;
 import com.cursedcauldron.unvotedandshelved.UnvotedAndShelved;
 import net.minecraft.data.DataGenerator;
 import net.minecraftforge.common.data.ExistingFileHelper;
+import net.minecraftforge.data.event.GatherDataEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.forge.event.lifecycle.GatherDataEvent;
 
 @Mod.EventBusSubscriber(modid = UnvotedAndShelved.MODID, bus = Mod.EventBusSubscriber.Bus.MOD)
 public class USDataGenerator {
--- src/main/java/com/cursedcauldron/unvotedandshelved/init/USEntityTypes.java
+++ src/main/java/com/cursedcauldron/unvotedandshelved/init/USEntityTypes.java
@@ -14,7 +14,7 @@ import net.minecraftforge.registries.RegistryObject;
 @Mod.EventBusSubscriber(modid = UnvotedAndShelved.MODID, bus = Mod.EventBusSubscriber.Bus.MOD)
 public class USEntityTypes {
 
-    public static final DeferredRegister<EntityType<?>> ENTITY_TYPES = DeferredRegister.create(ForgeRegistries.ENTITIES, UnvotedAndShelved.MODID);
+    public static final DeferredRegister<EntityType<?>> ENTITY_TYPES = DeferredRegister.create(ForgeRegistries.ENTITY_TYPES, UnvotedAndShelved.MODID);
 
     public static final RegistryObject<EntityType<GlareEntity>> GLARE = ENTITY_TYPES.register("glare", () -> EntityType.Builder.of(GlareEntity::new, MobCategory.UNDERGROUND_WATER_CREATURE).sized(0.8F, 1.2F).clientTrackingRange(8).build("glare"));
     public static final RegistryObject<EntityType<CopperGolemEntity>> COPPER_GOLEM = ENTITY_TYPES.register("copper_golem", () -> EntityType.Builder.of(CopperGolemEntity::new, MobCategory.CREATURE).sized(0.8F, 1.2F).clientTrackingRange(8).build("copper_golem"));
